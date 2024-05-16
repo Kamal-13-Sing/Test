ce481e84fb2fb193ca777a6f58c8f42a66d50030
Kamal-13-Sing <donbimal383@gmail.com>
2024-05-16 18:53:06 +0545

    third commit with merge

---
+++ src/main/java/com/mavenproject/Main.java
@@ -0,0 +1,17 @@
+package com.mavenproject;
+
+import service.SumExample;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int result  = SumExample.sum(10,40);
+
+        System.out.println("Sum is: "+result);
+
+
+        }
+
+}
